527482e997c635cbd3bb41ed69499cafa4a41de2
Pedrok212 <pedro_4x@outlook.com>
2026-03-04 11:51:45 -0300

    feat: atualiza o EstoqueController com o mock de dados

--- src/main/java/dev/pedrok/inventoryapi/Estoque/EstoqueController.java
+++ src/main/java/dev/pedrok/inventoryapi/Estoque/EstoqueController.java
@@ -8,11 +8,38 @@ import org.springframework.web.bind.annotation.*;
 public class EstoqueController {
 
     @GetMapping("/boasVindas")
-    @PostMapping
-    @PutMapping
-    @DeleteMapping
     public String boasVindas() {
         return "Primeira mensagem na rota";
     }
 
+    // Adicionar produto (CREATE)
+    @PostMapping("/criarProduto")
+    public String criarProduto(){
+        return "Produto criado";
+    }
+
+    // Procurar produtos tanto por nome, quanto por código(READ)
+    @GetMapping("/procura")
+    public String procurarProduto(){
+        return "Caçando produto";
+    }
+
+    //Mostrar os produtos por sessão, mercearia, congelados, higiene(READ)
+
+    @GetMapping("/produtos")
+    public String mostrarProduto(){
+        return "Mostrar produto por sessão";
+    }
+
+    // Alterar alguma coisa do produto caso seja necessário(UPDATE)
+    @PutMapping("/alterarInfoProduto")
+    public String alterarProduto(){
+        return "Alterar alguma coisa do produto";
+    }
+
+    // Deletar produto que a empresa não vende mais(DELETE)
+        @DeleteMapping("/excluirProduto")
+        public String excluirProduto(){
+            return "Tirar o produto de estoque";
+        }
 }
